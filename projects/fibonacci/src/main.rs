95264ea34506b08c04915fa52d7ffbf46ce75e2b
Pablo D. Garcia <54770806+pdgarci@users.noreply.github.com>
2023-08-20 21:20:44 -0300

    First fullz working version with robust input validation

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,28 +1,86 @@
 use std::io;
-use regex::Regex;
+use
+{
+    once_cell::sync::Lazy,
+    regex::Regex,
+};
+use std::{thread, time};
 
-fn main() {
+fn input() -> String
+{
+    let mut n = String::new();
+    println!("Enter n (q or Q to quit)");
+    io::stdin()
+        .read_line(&mut n)
+        .expect("Failed to read input");
+    return n.trim_end().to_string();
+}
+
+fn fibonacci(n: u8) -> u128
+{
+    if n<2
+    {
+        return n as u128;
+    }
+    let mut n0: u128 = 0;
+    let mut n1: u128 = 1;
+    let mut i: u8 = 2;
+    let mut fib: u128 = 0;
+    while i<=n
+    {
+        fib = n0 + n1;
+        n0 = n1;
+        n1 = fib;
+        i += 1;
+    }
+    fib
+}
+
+fn welcome()
+{
+    clearscreen::clear().expect("Failed to clear the screen");
     println!("This program calculates the Fibonacci sequence of n");
+}
 
-    let mut n = String::from("00");
-
-//    let mut n = String::new();
-    let re1 = Regex::new(r"^[qQ]").unwrap();
-
-//    let re2 = Regex::new(r"").unwrap();
-//    while n != String::from("q") {
-//    while n.as_bytes()[0] != (b'q') {
-//    while &n[0] != '0'.to_string() {
-//    while ![b'q', b'Q'].contains(&n.as_bytes()[0]) {
-//    while !((&n.as_bytes().contains(&b'q')) | (&n.as_bytes().contains(&b'Q'))) {
-
-//    while !re1.is_match(&n) && (&n).chars().count()>1 {
-    while (&n).chars().count()>1 {
-        n = "".to_string();
-        println!("Enter n (q or Q to quit)");
-        io::stdin()
-           .read_line(&mut n)
-           .expect("Failed to read input");
-//        println!("n={n}");
+fn main()
+{
+    static RE1: Lazy<Regex> = Lazy::new(|| Regex::new(r"^[qQ]").unwrap());
+    static RE2: Lazy<Regex> = Lazy::new(|| Regex::new(r"[^0-9]").unwrap());
+    static RE3: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\s*$)").unwrap());
+    welcome();
+    let mut n = input();
+    while !(RE1.is_match(&n) && (&n).chars().count()==1)
+    {
+        while RE2.is_match(&n) && !(RE1.is_match(&n) && (&n).chars().count()==1)
+        {
+            println!("\"{}\" is not a natural number!",&n);
+            thread::sleep(time::Duration::from_millis(800));
+            welcome();
+            n = input();
         }
+        if !(RE1.is_match(&n) && (&n).chars().count()==1)
+        {
+            if RE3.is_match(&n)
+            {
+                println!("Empty value entered!");
+                thread::sleep(time::Duration::from_millis(800));
+            }
+            else
+            {
+                let n_as_int = n.parse::<u8>().unwrap();
+                if n_as_int>186
+                {
+                    println!("Cannot calculate Fibonacci's sequence for n greater than 186");
+                    thread::sleep(time::Duration::from_millis(1800));
+                }
+                else
+                {
+                    println!("The Fibonacci number for {n} is {}",fibonacci(n_as_int));
+                    thread::sleep(time::Duration::from_millis(1800));
+                }
+            }
+            welcome();
+            n = input();
+        }
+    }
 }
